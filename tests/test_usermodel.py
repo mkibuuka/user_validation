ece4cd80ce0426c7e4fe7824bd91bf3e105704fc
myco <coditmyco6@gmail.com>
2018-09-26 14:47:09 +0300

    Add test_usermodel.py file [Finishes #160788890]

---
+++ tests/test_usermodel.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from app.user import User
+
+
+class TestcreateUser(TestCase):
+    """
+    Test class for testing instances of a user
+    """
+    def setUp(self):
+        self.new_user = User('michael', 'myco', 28, 'male', 'me@me.com', 'me12345')
+
+
+    def test_create_user(self):
+
+        self.assertEqual(self.new_user.username, 'myco')
+        self.assertEqual(self.new_user.email, 'me@me.com')
+        self.assertEqual(self.new_user.password, 'me12345')
+        self.assertIsInstance(self.new_user, User)
