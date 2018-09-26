fcb5ac3962a6026777584bec00ef06b59ef3f4b3
myco <coditmyco6@gmail.com>
2018-09-26 14:24:19 +0300

    Add user class model [Finishes #160788251]

---
+++ app/user.py
@@ -0,0 +1,16 @@
+class User(object):
+    """Class that defines attributes for a User object."""
+
+    class User(object):
+        """Class that defines attributes for a User object."""
+
+    users = {}
+
+    def __init__(self, name, username, age, gender, email, password):
+        self.name = name
+        self.username = username
+        self.age = age
+        self.gender = gender
+        self.email = email
+        self.password = password
+        self.active = False
