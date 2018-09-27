2049a7246b2485ff38f731057e85a531e086aa50
myco <coditmyco6@gmail.com>
2018-09-27 17:03:52 +0300

    Remove users dictionary from the user class

--- app/user.py
+++ app/user.py
@@ -4,9 +4,8 @@ class User(object):
     class User(object):
         """Class that defines attributes for a User object."""
 
-    users = {}
-
     def __init__(self, name, username, age, email, password):
+        self.user_id = 0
         self.name = name
         self.username = username
         self.age = age
